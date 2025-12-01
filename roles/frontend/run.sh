fb2f3cef68f1732a93590768dfa6e762715ce08f
kishore babu <shkishore8@gmail.com>
2025-12-01 13:40:28 +0530

    frontend config

---
+++ roles/frontend/run.sh
@@ -0,0 +1,6 @@
+git pull
+if [ -z "$1" ]; then
+  echo Input is expected
+  exit 1
+fi
+ansible-playbook -i ${1}-dev.shkishore.online, main.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=$1case $1

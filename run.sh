be70b0c3f05f3e43eb61c8adc1e58f6447ba2048
kishore babu <shkishore8@gmail.com>
2025-12-01 14:26:21 +0530

    fr

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
   echo Input is expected
   exit 1
 fi
-ansible-playbook -i dev.${1}shkishore.online, main.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=$1
+ansible-playbook -i dev.${1}.shkishore.online, main.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e component=$1
